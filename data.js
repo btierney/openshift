609cd9c54ac45be8942d28da58d7c5af701b6ed5
btierney <bob.tierney@verizon.net>
2018-08-25 09:10:05 -0700

    File List

---
+++ data.js
@@ -0,0 +1,89 @@
+var topics  = 
+    [
+        {
+            "id"            : 0,
+            "topic_title"   : "IT Optimization",
+            "descr"         : "Modernize your infrastructure, applications, processes and policies that help run and protect your business",
+            "videos"        : [
+                { "video_title" : "Configure Ansible with OpenSCAP" ,       "file_name" : "Redhat/IT Optimization/Ansible_ Scaling Security Configuration with OpenSCAP.mp4" },
+                { "video_title" : "Why Red Hat for Containers" ,            "file_name" : "Redhat/IT Optimization/OpenShift_ Why Red Hat for Containers.mp4" },
+                { "video_title" : "Modern Web Apps" ,                       "file_name" : "Redhat/IT Optimization/Red Hat Software Collections_ Modern Web Apps.mp4" },
+                { "video_title" : "Composer Overview" ,                     "file_name" : "Redhat/IT Optimization/RHEL_ Composer Overview.mp4" },
+                { "video_title" : "Create an Image Using Composer" ,        "file_name" : "Redhat/IT Optimization/RHEL_ Creating an Image Using Composer.mp4" },
+                { "video_title" : "Deploying SQL Server 2017 on RHEL" ,     "file_name" : "Redhat/IT Optimization/RHEL_ Deploying SQL Server 2017 on RHEL.mp4" },
+                { "video_title" : "Enterprise Choice for Public Clouds" ,   "file_name" : "Redhat/IT Optimization/RHEL_ Enterprise Choice for Public Clouds.mp4" },
+                { "video_title" : "Upgrade RHEL 6 to RHEL 7 In-Place" ,     "file_name" : "Redhat/IT Optimization/RHEL_ In-Place Upgrading RHEL 6 to RHEL 7.mp4" },
+                { "video_title" : "Managing Multiple Environments" ,        "file_name" : "Redhat/IT Optimization/RHEL_ Managing Multiple Environments with System Roles.mp4" },
+                { "video_title" : "Performance Profiles" ,                  "file_name" : "Redhat/IT Optimization/RHEL_ Performance Profiles.mp4" },
+                { "video_title" : "Web Console Registration and Updates" ,  "file_name" : "Redhat/IT Optimization/RHEL_ Registration and Updates via the Web Console.mp4" },
+                { "video_title" : "Relax and Recover" ,                     "file_name" : "Redhat/IT Optimization/RHEL_ Relax and Recover.mp4" },
+                { "video_title" : "RHEL for Real-Time" ,                    "file_name" : "Redhat/IT Optimization/RHEL_ RHEL for Real-Time.mp4" },
+                { "video_title" : "STRATIS for Storage Management" ,        "file_name" : "Redhat/IT Optimization/RHEL_ STRATIS for Storage Management.mp4" },
+                { "video_title" : "System Roles for Networking" ,           "file_name" : "Redhat/IT Optimization/RHEL_ System Roles for Networking.mp4" },
+                { "video_title" : "USB Device Whitelisting" ,               "file_name" : "Redhat/IT Optimization/RHEL_ USB Device Whitelisting.mp4" },
+                { "video_title" : "VDO Storage Optimization" ,              "file_name" : "Redhat/IT Optimization/RHEL_ VDO Storage Optimization.mp4" },
+                { "video_title" : "VM Troubleshooting with Web Console" ,   "file_name" : "Redhat/IT Optimization/RHEL_ VM Troubleshooting with Web Console.mp4" },
+            ]
+        },
+        {
+            "id"            : 1,
+            "topic_title"   : "Agile Integration",
+            "descr"         : "Easily connect different software applications, APIs, data, and devices to automate business processes and provide the right information at the right time",
+            "videos"        : [
+                { "video_title" : "3Scale API Managment" ,                                      "file_name" : "Redhat/Agile Integration/3Scale_ API Management.mp4" },
+                { "video_title" : "3Scale API Security with OpenID Connect" ,                   "file_name" : "Redhat/Agile Integration/3Scale_ API Security with OpenID Connect.mp4" },
+                { "video_title" : "Agile Integration Overview & Demo" ,                         "file_name" : "Redhat/Agile Integration/Agile Integration Overview and Demo.mp4" },
+                { "video_title" : "Agile Integration Overview" ,                                "file_name" : "Redhat/Agile Integration/Agile Integration_ Overview.mp4" },
+                { "video_title" : "AMQ Online - Getting Started" ,                              "file_name" : "Redhat/Agile Integration/AMQ Online_ Getting Started.mp4" },
+                { "video_title" : "Fuse 7.0 Features" ,                                         "file_name" : "Redhat/Agile Integration/Fuse 7.0_ Features.mp4" },
+                { "video_title" : "Fuse Online - Integrating a SaaS API" ,                      "file_name" : "Redhat/Agile Integration/Fuse Online_ Integrating a SaaS API.mp4" },
+                { "video_title" : "Process Automation Manager - Process Drive Applications" ,   "file_name" : "Redhat/Agile Integration/Process Automation Manager_ Process-Driven Applications.mp4" },
+            ]
+        },
+        {
+            "id"            : 2,
+            "topic_title"   : "Hybrid Cloud Infrastructure",
+            "descr"         : "Always-on, secure, scalable dynamic infrastructure to meet the needs of developers and the business",
+            "videos"        : [
+                { "video_title" : "Why Red Hat for Containers" ,                "file_name" : "Redhat/Hybrid Cloud Infrastructure/Hybrid Cloud Infrastructure - New Vids/OpenShift_ Why Red Hat for Containers.mp4" },
+                { "video_title" : "Enterprise Choice for Public Clouds" ,       "file_name" : "Redhat/Hybrid Cloud Infrastructure/Hybrid Cloud Infrastructure - New Vids/RHEL_ Enterprise Choice for Public Clouds.mp4" },
+                { "video_title" : "Provisioning a Security-Compliant Host" ,    "file_name" : "Redhat/Hybrid Cloud Infrastructure/CloudForms_ Provisioning a Security-Compliant Host.mp4" },
+                { "video_title" : "Multi-Cloud Container Platform" ,            "file_name" : "Redhat/Hybrid Cloud Infrastructure/Openshift_ Multi-Cloud Container Platform.mp4" },
+                { "video_title" : "Quay Overview" ,                             "file_name" : "Redhat/Hybrid Cloud Infrastructure/OpenShift_ Quay Overview.mp4" },
+            ]
+        },
+        {
+            "id"            : 3,
+            "topic_title"   : "Cloud-Native Application Development",
+            "descr"         : "See architectures, processes, and platforms needed to build modern applications and digitally transform applications",
+            "videos"        : [
+                { "video_title" : "API Security with OpenID Connect" ,                  "file_name" : "Redhat/Cloud-Native Application Development/3Scale_ API Security with OpenID Connect.mp4" },
+                { "video_title" : "Getting started with AMQ Online" ,                   "file_name" : "Redhat/Cloud-Native Application Development/AMQ Online_ Getting Started.mp4" },
+                { "video_title" : "Cloud-Native Develpment" ,                           "file_name" : "Redhat/Cloud-Native Application Development/Cloud-Native Development_ Overview.mp4" },
+                { "video_title" : "Integrating a SaaS API with Fuse Online" ,           "file_name" : "Redhat/Cloud-Native Application Development/Fuse Online_ Integrating a SaaS API.mp4" },
+                { "video_title" : "Introduction to OpenShift Application Runtimes" ,    "file_name" : "Redhat/Cloud-Native Application Development/Introduction to Red Hat OpenShift Application Runtimes.mp4" },
+                { "video_title" : "Modernizing Existing Applications" ,                 "file_name" : "Redhat/Cloud-Native Application Development/Modernizing Existing Applications.mp4" },
+                { "video_title" : "OpenShift Cloud Functions" ,                         "file_name" : "Redhat/Cloud-Native Application Development/OpenShift_ Cloud Functions.mp4" },
+                { "video_title" : "OpenShift Mobile Services" ,                         "file_name" : "Redhat/Cloud-Native Application Development/OpenShift_ Mobile Services.mp4" },
+                { "video_title" : "OpenShift Multi-Cloud Container Platform" ,          "file_name" : "Redhat/Cloud-Native Application Development/OpenShift_ Multi-Cloud Container Platform.mp4" },
+                { "video_title" : "Process-Driven-Applications" ,                       "file_name" : "Redhat/Cloud-Native Application Development/Process Automation Manager_ Process-Driven Applications.mp4" },
+                { "video_title" : "Micro Rules with Descision Maker" ,                  "file_name" : "Redhat/Cloud-Native Application Development/Red Hat Decision Manager_ Micro Rules.mp4" },
+                { "video_title" : "RHOAR - Building an APP " ,                          "file_name" : "Redhat/Cloud-Native Application Development/RHOAR_ Building an App Demo.mp4" },
+                { "video_title" : "Reactive development with Exclipse Vert.x" ,         "file_name" : "Redhat/Cloud-Native Application Development/RHOAR_ Eclipse Vert.x.mp4" },
+                { "video_title" : "RHOAR - Microprofile" ,                              "file_name" : "Redhat/Cloud-Native Application Development/RHOAR_ Microprofile.mp4" },
+            ]
+        },
+        {
+            "id"            : 4,
+            "topic_title"   : "IT Automation + Management",
+            "descr"         : "Rapidly and securely operate at enterprise scale, including containers, Kubernetes, cloud, and DevOps",
+            "videos"        : [
+                { "video_title" : "New features in Satellite 6.3" ,                     "file_name" : "Redhat/IT Automation + Management/Satellite 6.3_ New Features.mp4" },
+                { "video_title" : "Red Hat Insights (Intro)" ,                          "file_name" : "Redhat/IT Automation + Management/Red Hat Insights_ Introduction.mp4" },
+                { "video_title" : "Ansible Overview" ,                                  "file_name" : "Redhat/IT Automation + Management/Ansible_ Overview.mp4" },
+                { "video_title" : "Ansible Network Configuration Backup" ,              "file_name" : "Redhat/IT Automation + Management/Ansible_ Network Configuration Backup.mp4" },
+                { "video_title" : "Ansible Workflows" ,                                 "file_name" : "Redhat/IT Automation + Management/Ansible Tower_ Workflows.mov" },
+                { "video_title" : "Ansible Network Configuration Demo" ,                "file_name" : "Redhat/IT Automation + Management/Ansible Tower_ Network Configuration Backup Demo.mp4" } 
+            ]
+        }
+]
